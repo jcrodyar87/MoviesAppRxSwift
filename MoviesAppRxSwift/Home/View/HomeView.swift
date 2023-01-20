8ff074ee1c5388f11f8770a90b3ad6569d83e8e1
Juan Carlos Rodriguez Yarmas <jcry87@gmail.com>
2023-01-19 22:50:17 -0500

    Add RxSwift and UITableView

--- MoviesAppRxSwift/Home/View/HomeView.swift
+++ MoviesAppRxSwift/Home/View/HomeView.swift
@@ -8,11 +8,26 @@
 import UIKit
 
 class HomeView: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var activity: UIActivityIndicatorView!
     private var router = HomeRouter()
     private var viewModel = HomeViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         viewModel.bind(view:self, router: router)
+    }
+}
+
+extension HomeView: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        return cell
     }
 }
